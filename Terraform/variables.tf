ef909c21f57b57e9213b988588f93ace36654858
lanirelad <lanirelad@gmail.com>
2024-10-24 23:57:10 +0300

    added var definition

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -3,7 +3,7 @@ variable "linodeToken" {type = string}
 variable "sshKey" {type = string}
 variable "rootPass" {type = string}
 # variable "filename" {type = string}
+variable "githubToken" {type = string}
 
 
-# variable githubToken = "YOUR_GITHUB_PERSONAL_ACCESS_TOKEN"
 # variable webhookSecret = "YOUR_WEBHOOK_SECRET"  # A secure secret for the GitHub Webhook
